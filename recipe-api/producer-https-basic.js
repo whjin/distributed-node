a8cab76d1f7edb8d0fd3e33a983dfff5fd13aaaf
whjin <1414848321@qq.com>
2023-11-15 14:11:23 +0800

    update

--- recipe-api/producer-https-basic.js
+++ recipe-api/producer-https-basic.js
@@ -2,9 +2,9 @@
 
 const fs = require("fs");
 const server = require("fastify")({
-  https:{
-    key:fs.readFileSync(__dirname+'/tls/basic-private-key.key'),
-    
+  https: {
+    key: fs.readFileSync(__dirname + '/tls/basic-private-key.key'),
+    cert: fs.readFileSync(__dirname + '/../shared/tls/basic-certificate.cert')
   }
 });
 const HOST = process.env.HOST || "127.0.0.1";
@@ -24,16 +24,16 @@ server.get("/recipes/:id", async (req, res) => {
     producer_pid: process.pid,
     recipe: {
       id,
-      name: "Chicken Tikka Masala",
-      steps: "Throw it in a pot...",
+      name: "滕王阁饯别序",
+      steps: "豫章故郡，洪都新府",
       ingredients: [
-        { id: 1, name: "Chicken", quantity: "1 lb" },
-        { id: 2, name: "Sauce", quantity: "2 cups" },
+        { id: 1, name: "王勃", quantity: "李贺" },
+        { id: 2, name: "王维", quantity: "李煜" },
       ],
     },
   };
 });
 
 server.listen(PORT, HOST, () => {
-  console.log(`Producer running at http://${HOST}:${PORT}`);
+  console.log(`Producer running at https://${HOST}:${PORT}`);
 });
