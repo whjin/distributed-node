72fb3e6f1e7ed1b1fc7e634461a38e7369c46e41
whjin <wuhuajin09@163.com>
2023-12-03 23:18:10 +0800

    modified：调整静态文件目录

---
+++ public/js/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  host: "192.168.0.197",
+  port: 8443,
+};
--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const https = require("https");
 const fs = require("fs");
-const PORT = 8443;
+
+const config = require("./public/js/config");
+const HOST = process.env.HOST || config.host;
+const PORT = process.env.PORT || config.port;
+
 const options = {
   key: fs.readFileSync("certificate/server.key"),
   cert: fs.readFileSync("certificate/server.crt"),
 };
 
-fs.readFile("./public/index.html", (err, html) => {
+fs.readFile("./public/html/index.html", (err, html) => {
   if (err) {
     throw err;
   }
   https
-    .createServer(options, (req, res) =>{
+    .createServer(options, (req, res) => {
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(html);
       res.end();
     })
     .listen(8443, () => {
-      console.log(`App listening on port ${PORT}`);
+      console.log(`Server is running at https://${HOST}:${PORT}`);
     });
 });
