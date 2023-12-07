1045bd66b0a483b28e31c027c38d53385cffd46a
whjin <wuhuajin09@163.com>
2023-12-07 23:40:08 +0800

    update

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const express = require("express");
+const app = express();
 const https = require("https");
 const fs = require("fs");
 
@@ -10,17 +12,12 @@ const options = {
   cert: fs.readFileSync("certificate/server.crt"),
 };
 
-fs.readFile("./public/html/index.html", (err, html) => {
-  if (err) {
-    throw err;
-  }
-  https
-    .createServer(options, (req, res) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(html);
-      res.end();
-    })
-    .listen(8443, () => {
-      console.log(`Server is running at https://${HOST}:${PORT}`);
-    });
+const todoItems = [{ id: 0, value: "React", done: false, delete: false }];
+
+app.get("/items", (req, res) => {
+  res.send(todoItems);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://${HOST}:${PORT}`);
 });
